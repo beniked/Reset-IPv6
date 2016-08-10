0c81a4c736d55d8906d018e95b1972b339873688
beniked <beniked@users.noreply.github.com>
2016-08-10 10:14:40 -0400

    Update RS_ResetIPv6.ps1
    making it silent

--- RS_ResetIPv6.ps1
+++ RS_ResetIPv6.ps1
@@ -19,7 +19,7 @@ process {
 
  foreach ($Computer in $ComputerName) {            
   if(Test-Connection -ComputerName $Computer -Count 1 -ea 0) {            
-   Write-Verbose "$Computer is online"            
+#   Write-Verbose "$Computer is online"            
    $OS  = (Get-WmiObject -computername $computer -class Win32_OperatingSystem ).Caption            
    if ((Get-WmiObject -Class Win32_OperatingSystem -ComputerName $Computer -ea 0).OSArchitecture -eq '64-bit') {            
     $architecture = "64-Bit"            
@@ -40,7 +40,7 @@ end {}
 
 }
 
-Write-DiagProgress -Activity "Analyzing the system"
+# Write-DiagProgress -Activity "Analyzing the system"
 
 $arch =  Get-OSArchitecture -ComputerName localhost
 if($arch.Architecture -like "64-Bit")
@@ -52,35 +52,35 @@ else
  Copy-Item .\devcon32.exe C:\Windows\System32\devcon.exe
 }
 
-Write-DiagProgress -Activity "Uninstalling IPv6 interfaces" -Status "Tunnel adapater 6to4"
+#Write-DiagProgress -Activity "Uninstalling IPv6 interfaces" -Status "Tunnel adapater 6to4"
 devcon.exe remove *6to4mp
-Write-DiagProgress -Activity "Uninstalling IPv6 interfaces" -Status "Tunnel adapater Teredo"
+#Write-DiagProgress -Activity "Uninstalling IPv6 interfaces" -Status "Tunnel adapater Teredo"
 devcon.exe remove *teredo
-Write-DiagProgress -Activity "Uninstalling IPv6 interfaces" -Status "Tunnel adapter IP-HTTPS"
+#Write-DiagProgress -Activity "Uninstalling IPv6 interfaces" -Status "Tunnel adapter IP-HTTPS"
 devcon.exe remove *IPHTTPS
-Write-DiagProgress -Activity "Uninstalling IPv6 interfaces" -Status "Tunnel adapter Isatap"
+#Write-DiagProgress -Activity "Uninstalling IPv6 interfaces" -Status "Tunnel adapter Isatap"
 devcon.exe remove *ISATAP
 
 
 Set-Location $env:WINDIR\inf
-Write-DiagProgress -Activity "Installing IPv6 drivers"  -Status "Tunnel adapter 6to4"
-devcon.exe install nettun.inf *6to4mp
-Write-DiagProgress -Activity "Installing IPv6 drivers"  -Status "Tunnel adapter Teredo"
-devcon.exe install nettun.inf *teredo
-Write-DiagProgress -Activity "Installing IPv6 drivers"  -Status "Tunnel adapter IP-HTTPS"
-devcon.exe install nettun.inf *IPHTTPS
-Write-DiagProgress -Activity "Installing IPv6 drivers"  -Status "Tunnel adapter Isatap"
-devcon.exe install nettun.inf *ISATAP
+#Write-DiagProgress -Activity "Installing IPv6 drivers"  -Status "Tunnel adapter 6to4"
+#devcon.exe install nettun.inf *6to4mp
+#Write-DiagProgress -Activity "Installing IPv6 drivers"  -Status "Tunnel adapter Teredo"
+#devcon.exe install nettun.inf *teredo
+#Write-DiagProgress -Activity "Installing IPv6 drivers"  -Status "Tunnel adapter IP-HTTPS"
+#devcon.exe install nettun.inf *IPHTTPS
+#Write-DiagProgress -Activity "Installing IPv6 drivers"  -Status "Tunnel adapter Isatap"
+#devcon.exe install nettun.inf *ISATAP
 
-Write-DiagProgress -Activity "Resetting IPv6 settings" -Status "Netsh interface ipv6 reset"
+#Write-DiagProgress -Activity "Resetting IPv6 settings" -Status "Netsh interface ipv6 reset"
 netsh interface ipv6 reset
-Write-DiagProgress -Activity "Updating group policies" -Status "GPUpdate /Force"
+#Write-DiagProgress -Activity "Updating group policies" -Status "GPUpdate /Force"
 gpupdate /force /wait:0
 
-$input = Get-DiagInput -Id "WantToRestart"
-if($input -eq $true)
-{
-Restart-Computer
-} 
+#$input = Get-DiagInput -Id "WantToRestart"
+#if($input -eq $true)
+#{
+#Restart-Computer
+#} 
 
 Return 0
